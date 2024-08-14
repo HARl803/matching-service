dcaf0162480f964dbd1070f8ac0d6f7daba0af1b
fsm12 <fsm1204@naver.com>
2024-08-15 03:13:30 +0900

    :recycle: refactor[#2]: Enum에 한글매칭 추가하고 JPA로 적용될 수 있도록 수정

--- src/main/java/com/haribo/matching_service/global/enums/ReservationStatus.java
+++ src/main/java/com/haribo/matching_service/global/enums/ReservationStatus.java
@@ -1,11 +1,21 @@
 package com.haribo.matching_service.global.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum ReservationStatus {
-    RESERVATION_PENDING, // 예약대기
-    TIME_SELECTION,     // 시간선택
-    PAYMENT,            // 결제
-    CHAT_PENDING,       // 채팅대기
-    CHAT_ENTRY,         // 채팅입장
-    MATCHING_FAILED,    // 매칭실패
-    MATCHING_COMPLETED; // 매칭완료
+    RESERVATION_PENDING("예약대기"),
+    TIME_SELECTION("시간선택"),
+    PAYMENT("결제"),
+    CHAT_PENDING("채팅대기"),
+    CHAT_ENTRY("채팅입장"),
+    MATCHING_FAILED("매칭실패"),
+    MATCHING_COMPLETED("매칭완료");
+
+    private final String korean;
+
+    ReservationStatus(String korean) {
+        this.korean = korean;
+    }
+
 }
--- src/main/java/com/haribo/matching_service/mentee/domain/Reservation.java
+++ src/main/java/com/haribo/matching_service/mentee/domain/Reservation.java
@@ -1,20 +1,21 @@
 package com.haribo.matching_service.mentee.domain;
 
+import com.haribo.matching_service.global.converter.ReservationStatusConverter;
 import com.haribo.matching_service.global.entity.BaseTimeEntity;
 import com.haribo.matching_service.global.enums.ReservationStatus;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Entity
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Table(name = "Reservation")
+@Entity
+@Table(name = "reservation")
 public class Reservation extends BaseTimeEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "profile_id")
+    @Column(name = "reservation_id")
     private Integer reservationId;
 
     @Column(name = "mento_id", nullable = false)
@@ -23,6 +24,8 @@ public class Reservation extends BaseTimeEntity{
     @Column(name = "mentee_id", nullable = false)
     private String menteeId;
 
+    @Convert(converter = ReservationStatusConverter.class)
+    @Column(name = "status", nullable = false)
     private ReservationStatus status;
 
     private String request;
